a24c699db5f1db2d9b89c4504e004d52b38a56aa
Maciek Niemczyk <macilias@gmail.com>
2022-02-04 23:03:33 +0100

    CSRF

--- umfrageseite/polls/views.py
+++ umfrageseite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Poll
+from .models import Poll, Choice
 
 
 def index(request):
@@ -11,3 +11,16 @@ def index(request):
 def umfrage_detail(request, slug):
     context = {'umfrage': get_object_or_404(Poll, slug=slug)}
     return render(request=request, template_name='polls/umfrage.html', context=context)
+
+
+def vote(request, slug):
+    umfrage = get_object_or_404(Poll, slug=slug)
+    try:
+        selected = umfrage.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return HttpResponse("Fehler: es wurde keine bzw. eine ungültige Antwort ausgeählt!")
+    else:
+        selected.votes += 1
+        selected.save()
+        return HttpResponse("Super, du hast abgestimmt!")
+

f4ee4730a5dcb55e6f3a1444ab061a9aea569d97
Maciek Niemczyk <macilias@gmail.com>
2022-02-04 21:42:28 +0100

    a href v1

--- umfrageseite/polls/urls.py
+++ umfrageseite/polls/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import index, umfrage_detail
 
 urlpatterns = [
-    path('', index),
-    path('abstimmung/<str:slug>/', umfrage_detail),
+    path('', index, name="index"),
+    path('abstimmung/<str:slug>/', umfrage_detail, name="umfrage-detail"),
 ]

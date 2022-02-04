8e440a7cfef91337fa9ac5d3e8d6779e91c0e530
Maciek Niemczyk <macilias@gmail.com>
2022-02-04 23:02:12 +0100

    voting 1

--- umfrageseite/polls/urls.py
+++ umfrageseite/polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, umfrage_detail
+from .views import index, umfrage_detail, vote
 
 urlpatterns = [
     path('', index, name="index"),
-    path('abstimmung/<str:slug>/', umfrage_detail, name="umfrage-detail"),
+    path('poll/<str:slug>/', umfrage_detail, name="umfrage-detail"),
+    path('poll/<str:slug>/vote', vote, name="vote"),
 ]

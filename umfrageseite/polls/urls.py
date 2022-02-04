5cda6cb50c5d835d96e72b4a198da776350a78cb
Maciek Niemczyk <macilias@gmail.com>
2022-02-04 21:31:15 +0100

    extends block

--- umfrageseite/polls/urls.py
+++ umfrageseite/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, umfrage_detail
 
 urlpatterns = [
     path('', index),
+    path('abstimmung/<str:slug>/', umfrage_detail),
 ]
--- umfrageseite/polls/views.py
+++ umfrageseite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Poll
 
@@ -6,3 +6,8 @@ from .models import Poll
 def index(request):
     context = {'umfragen': Poll.objects.all()}
     return render(request=request, template_name='polls/index.html', context=context)
+
+
+def umfrage_detail(request, slug):
+    context = {'umfrage': get_object_or_404(Poll, slug=slug)}
+    return render(request=request, template_name='polls/umfrage.html', context=context)

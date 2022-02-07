fd9581d3dc558cb52f90cded82c091008a9619f9
Maciek Niemczyk <macilias@gmail.com>
2022-02-07 13:37:31 +0100

    added session check - before append error

--- umfrageseite/polls/views.py
+++ umfrageseite/polls/views.py
@@ -15,10 +15,30 @@ class PollDetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/umfrage.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_can_see_results = False
+        if 'voted_polls' in self.request.session:
+            if context['object'].id in self.request.session['voted_polls']:
+                user_can_see_results = True
+
+        context['voted'] = user_can_see_results
+        return context
+
 
 class ResultsDetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/results.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_can_see_results = False
+        if 'voted_polls' in self.request.session:
+            if context['object'].id in self.request.session['voted_polls']:
+                user_can_see_results = True
+
+        context['access'] = user_can_see_results
+        return context
 
 
 def vote(request, slug):
@@ -30,4 +50,12 @@ def vote(request, slug):
     else:
         selected.votes += 1
         selected.save()
+        if 'voted_polls' in request.session:
+            if type(request.session['voted_polls']) == list:
+                request.session['voted_polls'].append(umfrage.id)
+            else:
+                request.session['voted_polls'] = [umfrage.id]
+        else:
+            request.session['voted_polls'] = [umfrage.id]
+
         return HttpResponseRedirect(reverse('polls:results', args=(umfrage.slug,)))

bfae9cd2cf4c7f3d6dc3cd99c3e6809c54fdc9ab
Maciek Niemczyk <Maciek.Niemczyk@suena.energy>
2022-03-02 11:31:10 +0100

    moving on with chanells - fixed some dependency issues

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ This is a tutorial based on:
 https://www.udemy.com/course/django-kurs
 https://oc.v-gar.de/repo/django1-projekt-umfrageseite/blob/master/polls/urls.py
 
+To run redis in docker for suena1M use:
+$ docker run -p 6379:6379 -d redis:5
+Check if redis is running:
+$ redis-cli ping
+
+> PONG (expected result)
--- suena1M/suena1M/settings.py
+++ suena1M/suena1M/settings.py
@@ -72,6 +72,14 @@ TEMPLATES = [
 WSGI_APPLICATION = "suena1M.wsgi.application"
 
 ASGI_APPLICATION = "suena1M.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 APPEND_SLASH = False
 
